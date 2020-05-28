154ad1c2ab52fb2d907800d768855bc34d10469f
Armin Reichert <armin.reichert@web.de>
2020-05-28 13:09:09 +0200

    door sample used in Javadoc for statemachine

---
+++ Door/src/main/java/de/amr/statemachine/samples/door/Door.java
@@ -0,0 +1,40 @@
+package de.amr.statemachine.samples.door;
+
+import de.amr.statemachine.core.StateMachine.MissingTransitionBehavior;
+import de.amr.statemachine.samples.door.DoorController.DoorEvent;
+
+public class Door {
+
+	public static void main(String[] args) {
+		Door door = new Door();
+		door.unlock();
+		door.open();
+		door.open();
+		door.lock();
+		door.close();
+		door.unlock();
+	}
+
+	DoorController state = new DoorController();
+
+	public Door() {
+		state.setMissingTransitionBehavior(MissingTransitionBehavior.LOG);
+		state.init();
+	}
+
+	public void open() {
+		state.process(DoorEvent.OPEN_DOOR);
+	}
+
+	public void close() {
+		state.process(DoorEvent.CLOSE_DOOR);
+	}
+
+	public void lock() {
+		state.process(DoorEvent.LOCK_DOOR);
+	}
+
+	public void unlock() {
+		state.process(DoorEvent.UNLOCK_DOOR);
+	}
+}
---
+++ Door/src/main/java/de/amr/statemachine/samples/door/DoorController.java
@@ -0,0 +1,41 @@
+package de.amr.statemachine.samples.door;
+
+import static de.amr.statemachine.samples.door.DoorController.DoorEvent.CLOSE_DOOR;
+import static de.amr.statemachine.samples.door.DoorController.DoorEvent.LOCK_DOOR;
+import static de.amr.statemachine.samples.door.DoorController.DoorEvent.OPEN_DOOR;
+import static de.amr.statemachine.samples.door.DoorController.DoorEvent.UNLOCK_DOOR;
+import static de.amr.statemachine.samples.door.DoorController.DoorState.CLOSED;
+import static de.amr.statemachine.samples.door.DoorController.DoorState.LOCKED;
+import static de.amr.statemachine.samples.door.DoorController.DoorState.OPEN;
+
+import de.amr.statemachine.api.EventMatchStrategy;
+import de.amr.statemachine.core.StateMachine;
+import de.amr.statemachine.samples.door.DoorController.DoorEvent;
+import de.amr.statemachine.samples.door.DoorController.DoorState;
+
+public class DoorController extends StateMachine<DoorState, DoorEvent> {
+
+	public enum DoorState {
+		OPEN, CLOSED, LOCKED
+	}
+
+	public enum DoorEvent {
+		OPEN_DOOR, CLOSE_DOOR, LOCK_DOOR, UNLOCK_DOOR
+	}
+
+	public DoorController() {
+		super(DoorState.class, EventMatchStrategy.BY_EQUALITY);
+		//@formatter:off
+		beginStateMachine()
+			.initialState(LOCKED)
+			.description("Door")
+		.states()
+		.transitions()
+			.when(LOCKED).then(CLOSED).on(UNLOCK_DOOR)
+			.when(CLOSED).then(LOCKED).on(LOCK_DOOR)
+			.when(CLOSED).then(OPEN).on(OPEN_DOOR)
+			.when(OPEN).then(CLOSED).on(CLOSE_DOOR)
+		.endStateMachine();
+		//@formatter:on
+	}
+}
